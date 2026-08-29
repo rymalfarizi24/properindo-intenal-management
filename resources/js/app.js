07e4859b14044c108c4cc573250b1014040d220d
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-08-30 05:06:22 +0700

    fix: remove trix and vanilla picker

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,1 @@
 import './bootstrap';
-import Trix from "trix"
-import Picker from 'vanilla-picker';
-
-window.Picker = Picker;
-
-document.addEventListener("trix-before-initialize", () => {
-  // Change Trix.config if you need
-})
